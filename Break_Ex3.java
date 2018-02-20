ce3a7a264ba6b7fa4816cbdddbcabb8a14ffb6c8
CRAIG HEFFRON - STUDENT <G00349323@gmit.ie>
2018-02-20 13:50:15 +0000

    week 20 lab ex 3

---
+++ Break_Ex3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Break_Ex3{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int counter = 0;
+
+    System.out.println("Guess a number from 1 to 10");
+    System.out.println("You have 10 attemps:");
+
+    while(input.hasNext()){
+    counter++;
+      if((input.nextInt() == 8) || (counter == 10));
+        System.out.println("Game Over");
+        break;
+      }
+    }
+  }
